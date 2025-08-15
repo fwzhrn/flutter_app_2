bbe50015cf820168d6db8d97473e9ef07d9e6393
fwzhrn <fwzhrn@gmail.com>
2025-08-15 15:53:19 +0700

    ppp

---
+++ lib/profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Expanded(flex: 2, child: _TopPortion()),
+          Expanded(flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0)
+            ),)
+        ],
+      ),
+    )
+  }
+}
